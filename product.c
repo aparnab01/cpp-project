39d50f58ce64662f41d78d75b07b5c003f3978aa
aparnab01 <78951807+aparnab01@users.noreply.github.com>
2021-02-12 14:13:46 +0530

    product of two number
    printing product of two numbers

---
+++ product.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    float a,b,c;
+    cout<<"Enter two numbers"<<endl;
+    cin>>a>>b;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    c=(a*b);
+    cout<<"Product="<<c<<endl;
+    return 0;
+}
